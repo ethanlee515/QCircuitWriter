98b4fb4ede39c845e2469fc47023ed26d92f4a64
Yi Lee <ylee1228@umd.edu>
2023-10-01 01:00:27 -0400

    Implement ellipsis

--- qcircuit.py
+++ qcircuit.py
@@ -27,11 +27,29 @@ class Output:
     def __init__(self, value):
         self.value = value
 
-class WireStatus(Enum):
+class WireType(Enum):
     ON = 0
     OFF = 1
-    ELLIPSIS = 2
-    CLASSICAL = 3
+    CLASSICAL = 2
+
+class WireStatus():
+    def __init__(self):
+        self.off()
+
+    def qw(self):
+        self.wire_type = WireType.ON
+        self.ellipsis = False
+
+    def cw(self):
+        self.wire_type = WireType.CLASSICAL
+        self.ellipsis = False
+
+    def off(self):
+        self.wire_type = WireType.OFF
+        self.ellipsis = False
+
+    def etc(self):
+        self.ellipsis = True
 
 class QCircuit:
     def __init__(self, num_wires):
@@ -59,49 +77,60 @@ class QCircuit:
         for w in column:
             content = column[w]
             if isinstance(content, Ctrl) and w == wire:
-                wire_status[wire] = WireStatus.ON
+                wire_status[wire].qw()
                 return f"\\ctrl{{{content.target}}}"
             elif isinstance(content, Gate):
                 if w == wire:
                     if content.size == 1:
-                        wire_status[wire] = WireStatus.ON
+                        wire_status[wire].qw()
                         return f"\\gate{{{content.name}}}"
                     else:
-                        wire_status[wire] = WireStatus.ON
+                        wire_status[wire].qw()
                         return f"\\multigate{{{content.size - 1}}}{{{content.name}}}"
                 elif wire > w and wire < w + content.size:
-                    wire_status[wire] = WireStatus.ON
+                    wire_status[wire].qw()
                     return f"\\ghost{{{content.name}}}"
             elif isinstance(content, Input) and w == wire:
-                wire_status[wire] = WireStatus.ON
+                wire_status[wire].qw()
                 return f"\\lstick{{{content.value}}}"
             elif isinstance(content, Output) and w == wire:
-                if wire_status[wire] == WireStatus.ON:
+                if wire_status[wire].wire_type == WireType.ON:
                     x = r"\qw"
                 else:
                     x = r"\cw"
-                wire_status[wire] = WireStatus.OFF
+                wire_status[wire].off()
                 return f"\\rstick{{{content.value}}}{x}"
-            elif isinstance(content, Etc) and w == wire:
-                wire_status[wire] = WireStatus.ELLIPSIS
-                return f"\\cdots"
             elif isinstance(content, Measure) and w == wire:
-                wire_status[wire] = WireStatus.CLASSICAL
+                wire_status[wire].cw()
                 return f"\\meter"
+            elif isinstance(content, Etc):
+                wire_status[wire].etc()
+                if w == wire:
+                    return f"\\cdots"
+                else:
+                    return ""
         # Content not found - maybe just a wire?
-        if wire_status[wire] == WireStatus.ON:
-            return r"\qw"
-        elif wire_status[wire] == WireStatus.CLASSICAL:
-            return r"\cw"
-        elif wire_status[wire] == WireStatus.ELLIPSIS:
-            wire_status[wire] = WireStatus.ON
-            return ""
+        wiretype = wire_status[wire].wire_type;
+        if wiretype == WireType.ON:
+            if wire_status[wire].ellipsis:
+                wire_status[wire].ellipsis = False
+                return ""
+            else:
+                return r"\qw"
+        elif wiretype == WireType.CLASSICAL:
+            if wire_status[wire].ellipsis:
+                wire_status[wire].ellipsis = False
+                return ""
+            else:
+                return r"\cw"
         else:
-            assert wire_status[wire] == WireStatus.OFF
+            assert wiretype == WireType.OFF
             return ""
 
     def getLaTeX(self):
-        wire_status = [WireStatus.OFF] * self.num_wires
+        wire_status = list()
+        for i in range(self.num_wires):
+            wire_status.append(WireStatus())
         lines = [self.getLaTeXforCell(0, i, wire_status)
                     for i in range(self.num_wires)]
         for c in range(1, len(self)):
@@ -143,7 +172,25 @@ if __name__ == "__main__":
     ct.insert(6, Ctrl(-5), t)
     t += 1
     ct.insert(1, Gate("H"), t)
-
-
+    # Attack 2
+    t += 1
+    ct.insert(1, Gate("U_2", 6), t)
+    # Measure 2 and output 2
+    t += 1
+    ct.insert(1, Measure(), t)
+    t += 1
+    ct.insert(1, Output("b_2"), t)
+    # Ellipsis
+    t += 1
+    ct.insert(3, Etc(), t)
+    # delta t
+    t += 1
+    ct.insert(7, Input(r"\ket{\delta_t}"), t)
+    # Honest behavior T
+    # TODO LaTeX macro?
+    t += 1
+    ct.insert(2, Gate(r"T^{\delta_t}"), t)
+    ct.insert(7, Ctrl(-5), t)
+    
 
     print(ct.getLaTeX())
