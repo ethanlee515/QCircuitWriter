73a7d591eb1c56b69d44053308b81219726bcdae
Yi Lee <ylee1228@umd.edu>
2023-10-01 22:49:54 -0400

    Implement more wire customizations

--- qcircuit.py
+++ qcircuit.py
@@ -10,6 +10,12 @@ class Measure:
 class Etc:
     pass
 
+class MultipleWires:
+    pass
+
+class WireOff:
+    pass
+
 class Ctrl:
     def __init__(self, target):
         self.target = target
@@ -109,6 +115,12 @@ class QCircuit:
                     return f"\\cdots"
                 else:
                     return ""
+            elif isinstance(content, MultipleWires) and w == wire:
+                wire_status[wire].qw()
+                return r"{/} \qw"
+            elif isinstance(content, WireOff) and w == wire:
+                wire_status[wire].off()
+                return ""
         # Content not found - maybe just a wire?
         wiretype = wire_status[wire].wire_type;
         if wiretype == WireType.ON:
@@ -137,60 +149,3 @@ class QCircuit:
             for i in range(self.num_wires):
                 lines[i] += f" & {self.getLaTeXforCell(c, i, wire_status)}"
         return functools.reduce(lambda a, b: f"{a} \\\\\n{b}", lines)
-
-if __name__ == "__main__":
-    ct = QCircuit(8)
-    # Inputs
-    t = 0
-    ct.insert(4, Input(r"\ket{0}"), t)
-    # Setting up the graph edges
-    t += 1
-    ct.insert(0, Ctrl(1), t)
-    ct.insert(1, Ctrl(1), t)
-    ct.insert(2, Ctrl(1), t)
-    ct.insert(3, Ctrl(-1), t)
-    ct.insert(5, Input(r"\ket{\delta_1}"), t)
-    # Honest behavior 1
-    t += 1
-    ct.insert(0, Gate(r"T^{\delta_1}"), t)
-    ct.insert(5, Ctrl(-5), t)
-    t += 1
-    ct.insert(0, Gate("H"), t)
-    # Attack 1
-    t += 1
-    ct.insert(0, Gate("U_1", 6), t)
-    # Measure 1
-    t += 1
-    ct.insert(0, Measure(), t)
-    # output and new delta
-    t += 1
-    ct.insert(0, Output("b_1"), t)
-    ct.insert(6, Input(r"\ket{\delta_2}"), t)
-    # Honest behavior 2
-    t += 1
-    ct.insert(1, Gate(r"T^{\delta_2}"), t)
-    ct.insert(6, Ctrl(-5), t)
-    t += 1
-    ct.insert(1, Gate("H"), t)
-    # Attack 2
-    t += 1
-    ct.insert(1, Gate("U_2", 6), t)
-    # Measure 2 and output 2
-    t += 1
-    ct.insert(1, Measure(), t)
-    t += 1
-    ct.insert(1, Output("b_2"), t)
-    # Ellipsis
-    t += 1
-    ct.insert(3, Etc(), t)
-    # delta t
-    t += 1
-    ct.insert(7, Input(r"\ket{\delta_t}"), t)
-    # Honest behavior T
-    # TODO LaTeX macro?
-    t += 1
-    ct.insert(2, Gate(r"T^{\delta_t}"), t)
-    ct.insert(7, Ctrl(-5), t)
-    
-
-    print(ct.getLaTeX())
